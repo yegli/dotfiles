9fb0fde43a1c53370438e941d040482e8caee2df
Yanick Egli <nick.eglirohr@gmail.com>
2026-04-29 16:58:44 +0200

    update dock

--- run_onchange_08-dock-setup.sh
+++ run_onchange_08-dock-setup.sh
@@ -10,10 +10,12 @@ echo "Configuring Dock..."
 dockutil --remove all --no-restart
 
 # ── Apps (add in desired order after Finder) ─────────────────────────────────
+dockutil --add /System/Applications/Mission\ Control.app                      --no-restart
 dockutil --add /Applications/Google\ Chrome.app                               --no-restart
 dockutil --add /System/Applications/Calendar.app                              --no-restart
 dockutil --add /System/Applications/Reminders.app                             --no-restart
 dockutil --add /System/Applications/Photos.app                                --no-restart
+dockutil --add /System/Applications/System\ Settings.app                                --no-restart
 # Add more apps here ↓
 
 # ── Right side: Applications folder as a stack ────────────────────────────────
